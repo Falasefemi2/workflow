78e547569e45eca01c48e65192822d83a38e7af4
Falasw femi <femifalase228@gmail.com>
2026-02-13 15:48:39 +0100

    feat: Login page forget password page reset password page

---
+++ src/auth/forgot-password-form.tsx
@@ -0,0 +1,60 @@
+import { cn } from "@/lib/utils";
+import type React from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { NavLink } from "react-router";
+
+export function ForgotPasswordForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8">
+            <FieldGroup>
+              <div className="flex flex-col items-center gap-2 text-center">
+                <h1 className="text-2xl font-bold">Password React</h1>
+                <p className="text-muted-foreground text-balance">
+                  Enter the email address assoicated to your account below.
+                </p>
+              </div>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <NavLink
+                    to="/"
+                    className="ml-auto text-sm underline-offset-2 hover:underline"
+                  >
+                    Login Instead
+                  </NavLink>
+                </div>
+              </Field>
+              <Field>
+                <Button type="submit">Password Reset</Button>
+              </Field>
+            </FieldGroup>
+          </form>
+          <div className="bg-muted relative hidden md:block">
+            <img
+              src="/placeholder.svg"
+              alt="Image"
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
